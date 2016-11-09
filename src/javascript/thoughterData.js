fad27f3b3a0c924b17bb86909227504313f44e80
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-09 15:35:10 -0500

    got a POST ajax call for the submit of a 'new thought'

--- src/javascript/thoughterData.js
+++ src/javascript/thoughterData.js
@@ -2,4 +2,26 @@
   'use strict';
   window.thoughter = window.thoughter || {};
 
+  $('.submit-form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: 'https://thoughter.herokuapp.com/api/Thoughts',
+      method: 'POST',
+      dataType: 'json',
+      data: JSON.stringify({
+        'content': ''
+      }),
+      headers: {
+        'content-type':'application/json'
+      }
+
+    })
+    .done(function handleSuccess(data){
+      console.log('success', data);
+    })
+    .fail(function handleFailure(xhr){
+      console.log('failed', xhr);
+    });
+  });
+
 }());
