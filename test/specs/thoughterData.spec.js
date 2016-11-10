ac5282892343285df3322c0a2efb2e3a57041677
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-10 16:39:33 -0500

    Started the ajax call test for the POST

--- test/specs/thoughterData.spec.js
+++ test/specs/thoughterData.spec.js
@@ -6,13 +6,38 @@
     var expect = chai.expect
     var fixtures = $('#fixtures').html();
 
-    describe('Thoughter Creation', function (){
+
+    describe('Posting new thought', function(){
+      it('Should post new thought to the server', function(){
+
+      });
+    });
+
+  describe('Thoughter Creation', function (){
 
         it('Should know that the namespace exists', function(){
           expect(window.thoughter).to.be.a('object')
         });
+        describe('Create Thought', function (){
+          it('should create a thought when submitted', function(done) {
+            console.log(window.thoughter.createThought('this is a thought'));
+          var xhr = window.thoughter.createThought();
+            expect(xhr).to.be.an('object');
+            expect(xhr.done).to.be.a('function');
+            expect(xhr.fail).to.be.a('function');
+
+            xhr
+              .done(function handleSuccess(data){
+                expect(data).to.be.a('function');
+                done();
+              })
+              .fail(function handleFailure(){
+                done('nope');
+              })
+          })
+        });
 
     });
 
-    
+
 }());
